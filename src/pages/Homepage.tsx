5046c44ce659d3720c1996eb9e94f65e35d488e3
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-05-11 16:36:04 -0700

    pulling commit

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-const HomePage: React.FC = () => {
-    return(
-        <div>
-            <h1>This is homepage</h1>
-        </div>
-    )     
-}
+
+const HomePage = () => {
+    return (
+        <div></div>
+    )
+};
+
 export default HomePage;
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,13 +1,18 @@
 import { Switch, Route } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
+import  Navbar  from '../components/Navbar';
 
 const AppRouter = () => {
     return (
+
         <div className='container'>
+          <div>
+          <Navbar/>
           <Switch>
             <Route path='/' exact component={Homepage} />
             <Route/>
           </Switch>
+        </div>
         </div>
      
     );
