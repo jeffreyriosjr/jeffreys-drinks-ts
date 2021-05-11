5b1ff247592b7fb6ba5652167f5e9598049b6f85
meru20 <merhawitgubsa@gmail.com>
2021-05-11 14:53:11 -0700

    feat(AppRouter): imported Navbar to the AppRouter

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,9 +1,11 @@
 import { Switch, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
+import HomePage from '../pages/Homepage';
+import Navbar from '../components/Navbar';
 
 const AppRouter = () => {
     return (
         <div className='container'>
+          <Navbar/>
           <Switch>
             <Route path='/' exact component={HomePage} />
             <Route path='/cocktails/:cocktailId'/>
