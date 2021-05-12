655eeab984228a2231e9cc270c597763022739e4
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-05-12 15:32:30 -0700

    feat(single-card): created single drink page using single card link to print

---
+++ src/components/SingleDrinkComponent.tsx
@@ -0,0 +1,10 @@
+
+const SingleDrinkComponent = () => {
+    return (
+        <div className='col text-center'>
+            <h2>MERU</h2>
+        </div>
+    )
+};
+
+export default SingleDrinkComponent;
