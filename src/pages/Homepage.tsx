5e2a0c18d9e2fdb74241a6f3217f0c2894e3805b
meru20 <merhawitgubsa@gmail.com>
2021-05-12 16:32:20 -0700

    fix(homepage): fix the typo error for the homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,38 +1,8 @@
-import {useEffect, useContext} from 'react';
-import {GlobalContext} from '../context/GlobalContext';
-import '../App.css';
-import DrinksCard from '../components/DrinksCard';
-const Homepage = () => {
-    const { getDrinks, drinks} = useContext(GlobalContext)
-    
-    useEffect(() => {
-        getDrinks() ;
-     
-      },[])
-    return (
-    <div id='home'>
-        <div className='row text-center'>
-            <div className='col'>
-                <h2>Home page</h2>
-                <h3>You have {drinks.length} items to sell</h3>
-            </div>
-        </div>
-        {/*** Drink map */}
-        <div className= 'row '>
-        {drinks.map((drink,index)=>{
-                return(
-                    <div className= 'col-sm-12 col-md-3 mb-3' key={index}>
-                        {/***drink card */}
-                        <DrinksCard drink={drink}/>
-                    </div>
-                )
-            })}
-            
-            
-    </div>
 
- </div>
+const HomePage = () => {
+    return (
+        <div></div>
     )
 };
 
-export default Homepage;
+export default HomePage;
