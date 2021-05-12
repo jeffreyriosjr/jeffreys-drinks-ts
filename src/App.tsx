b9186fb14ef5ef1eda4cebea80cffb564601def2
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-05-11 18:39:54 -0700

    Feat(types): created data types from the api references, added initial state types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './routers/AppRouter';
-<<<<<<< HEAD
 
 const App = () => {
   return (
@@ -13,13 +12,3 @@ const App = () => {
 };
 
 export default App;
-=======
-const App = () => {
-  return (
-    <Router>
-        <AppRouter />
-    </Router>
-  );
-};
-export default App;
->>>>>>> 998559b24a5036d90de4038894006363efba2212
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,7 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-<<<<<<< HEAD
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-5">
-=======
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
->>>>>>> 998559b24a5036d90de4038894006363efba2212
             {/*logo*/}
             <Link to='/' className='navbar-brand'>Drinks</Link>
             {/*menu*/}
